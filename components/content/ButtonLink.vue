7670f83cffc70b81d6ee7af5d09b0895c1da655d
r4nd3l <matemolnar88@gmail.com>
2024-02-25 12:22:53 +0100

    work tree wip

--- components/content/ButtonLink.vue
+++ components/content/ButtonLink.vue
@@ -1,4 +1,17 @@
-<script setup>
+<template>
+  <span class="not-prose">
+    <NuxtLink
+      :to="href"
+      :external="external"
+      class="inline-flex items-center gap-1 text-white bg-gray-800 dark:bg-gray-200 dark:text-gray-950 px-3 py-1 rounded no-underline"
+    >
+      <Icon v-if="icon" :name="icon" class="w-4 h-4" />
+      <ContentSlot :use="$slots.default" unwrap="p" />
+    </NuxtLink>
+  </span>
+</template>
+
+<script lang="ts" setup>
 defineProps({
   href: {
     type: String,
@@ -10,16 +23,7 @@ defineProps({
   },
   icon: {
     type: String,
-    default: '',
+    default: "",
   },
-})
+});
 </script>
-
-<template>
-  <span class="not-prose">
-    <NuxtLink :to="href" :external="external" class="inline-flex items-center gap-1 text-white bg-gray-800 dark:bg-gray-200 dark:text-gray-950 px-3 py-1 rounded no-underline">
-      <Icon v-if="icon" :name="icon" class="w-4 h-4" />
-      <ContentSlot :use="$slots.default" unwrap="p" />
-    </NuxtLink>
-  </span>
-</template>
